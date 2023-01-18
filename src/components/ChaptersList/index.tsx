25aa87da53258659e101857d4091d4eda8f09fcc
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-01-18 23:24:24 +0530

    expand and collapse animation for chapters list (sidebar)

--- src/components/ChaptersList/index.tsx
+++ src/components/ChaptersList/index.tsx
@@ -20,33 +20,39 @@ const ChaptersList = ({
   };
 
   return (
-    <div className={expanded ? styles.container : styles.hidden}>
-      <div className={styles.header}>
-        <>Chapters</>
-        <svg
-          className={styles.collapseIcon}
-          onClick={handleCollapse}
-          height="30px"
-          width="30px"
-          viewBox="0 0 16 16"
-          fill="white"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
-        </svg>
-      </div>
-      <div className={styles.chaptersList}>
-        {chapters.map(({ chapterName }, index: number) => (
-          <div
-            key={index}
-            className={styles.chapterName}
-            onClick={() => handleChapterClicked(chapterName)}
+    <>
+      <div
+        className={
+          expanded ? styles.container : `${styles.hidden} ${styles.container}`
+        }
+      >
+        <div className={styles.header}>
+          <>Chapters</>
+          <svg
+            className={styles.collapseIcon}
+            onClick={handleCollapse}
+            height="30px"
+            width="30px"
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            {chapterName}
-          </div>
-        ))}
+            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+          </svg>
+        </div>
+        <div className={styles.chaptersList}>
+          {chapters.map(({ chapterName }, index: number) => (
+            <div
+              key={index}
+              className={styles.chapterName}
+              onClick={() => handleChapterClicked(chapterName)}
+            >
+              {chapterName}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div className={expanded ? styles.overlay : styles.invisible} />
+    </>
   );
 };
 
