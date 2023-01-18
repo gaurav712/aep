b80ce4a6ef3bb5bbdb6937206bbdbf4cd0d6c68d
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-01-18 23:12:49 +0530

    header expand and collapse animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -87,7 +87,13 @@ const Header = ({
               <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
             </svg>
           </div>
-          <div className={headerExpanded ? styles.container : styles.hidden}>
+          <div
+            className={
+              headerExpanded
+                ? styles.container
+                : `${styles.hidden} ${styles.container}`
+            }
+          >
             <select
               className={styles.selectPicker}
               onChange={(event: ChangeEvent<HTMLSelectElement>) =>
@@ -136,6 +142,7 @@ const Header = ({
               <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
             </svg>
           </div>
+          <div className={headerExpanded ? styles.overlay : styles.invisible} />
         </>
       )}
     </SelectionContext.Consumer>
