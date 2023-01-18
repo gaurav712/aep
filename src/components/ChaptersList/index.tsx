fb51093a030d3109a9194c38686f46a4a6c84d00
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-01-18 15:05:10 +0530

    Sidebar text and color changes

--- src/components/ChaptersList/index.tsx
+++ src/components/ChaptersList/index.tsx
@@ -16,17 +16,21 @@ const ChaptersList = ({
         <>Chapters</>
         <svg
           onClick={handleCollapse}
-          height="20px"
-          width="20px"
+          height="30px"
+          width="30px"
           viewBox="0 0 48 48"
           fill="#ffffff"
         >
           <path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" />
         </svg>
       </div>
-      {chapters.map(({ chapterName }, index: number) => (
-        <div key={index}>{chapterName}</div>
-      ))}
+      <div className={styles.chaptersList}>
+        {chapters.map(({ chapterName }, index: number) => (
+          <div key={index} className={styles.chapterName}>
+            {chapterName}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
