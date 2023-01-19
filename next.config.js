4628ac70c96867f2cb57ff64fdffbb57b1abb866
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-01-19 08:10:03 +0530

    add basePath for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/aktu-exam-preparation",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
