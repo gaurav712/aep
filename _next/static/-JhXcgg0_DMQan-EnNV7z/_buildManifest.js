3e4d0570b4a8851e19a8c2b35b0d259048fd4cf3
gaurav712 <gaurav712@protonmail.com>
2023-01-20 15:05:50 +0000

    Deploying to gh-pages from @ gaurav712/aep@480e92b10d4fc8ea90c637df131539233cfac80b 🚀

---
+++ _next/static/-JhXcgg0_DMQan-EnNV7z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/39-844336bf160bafe5.js","static/css/d87e362ac4d87084.css","static/chunks/pages/index-5840c252be6fc047.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/RoimTMUiM8aG4XSnE6d1r/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/39-844336bf160bafe5.js","static/css/c3c2299000f37ab3.css","static/chunks/pages/index-5840c252be6fc047.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
