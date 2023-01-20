baa22c3fdacf5a0bdc20565270561d7a87f2e32a
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-01-20 15:02:26 +0530

    hide sidebar toggle button when no subject is selected

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -67,15 +67,19 @@ const Header = ({
       {(selectionContext) => (
         <>
           <div className={styles.collapedContainer}>
-            <svg
-              className={styles.expandIcon}
-              onClick={sidebarExpandHandler}
-              height="30px"
-              width="30px"
-              viewBox="0 0 48 48"
-            >
-              <path d="M7.5 36q-.65 0-1.075-.425Q6 35.15 6 34.5q0-.65.425-1.075Q6.85 33 7.5 33h33q.65 0 1.075.425Q42 33.85 42 34.5q0 .65-.425 1.075Q41.15 36 40.5 36Zm0-10.5q-.65 0-1.075-.425Q6 24.65 6 24q0-.65.425-1.075Q6.85 22.5 7.5 22.5h33q.65 0 1.075.425Q42 23.35 42 24q0 .65-.425 1.075-.425.425-1.075.425Zm0-10.5q-.65 0-1.075-.425Q6 14.15 6 13.5q0-.65.425-1.075Q6.85 12 7.5 12h33q.65 0 1.075.425Q42 12.85 42 13.5q0 .65-.425 1.075Q41.15 15 40.5 15Z" />
-            </svg>
+            {selectionContext?.selection.subject ? (
+              <svg
+                className={styles.expandIcon}
+                onClick={sidebarExpandHandler}
+                height="30px"
+                width="30px"
+                viewBox="0 0 48 48"
+              >
+                <path d="M7.5 36q-.65 0-1.075-.425Q6 35.15 6 34.5q0-.65.425-1.075Q6.85 33 7.5 33h33q.65 0 1.075.425Q42 33.85 42 34.5q0 .65-.425 1.075Q41.15 36 40.5 36Zm0-10.5q-.65 0-1.075-.425Q6 24.65 6 24q0-.65.425-1.075Q6.85 22.5 7.5 22.5h33q.65 0 1.075.425Q42 23.35 42 24q0 .65-.425 1.075-.425.425-1.075.425Zm0-10.5q-.65 0-1.075-.425Q6 14.15 6 13.5q0-.65.425-1.075Q6.85 12 7.5 12h33q.65 0 1.075.425Q42 12.85 42 13.5q0 .65-.425 1.075Q41.15 15 40.5 15Z" />
+              </svg>
+            ) : (
+              <div />
+            )}
             <svg
               className={styles.expandIcon}
               onClick={handleExpand}
