6816696c18ff17c25a533a05b855c526e20b11f8
gaurav712 <gaurav712@protonmail.com>
2023-01-21 10:13:14 +0000

    Deploying to gh-pages from @ gaurav712/aep@caed8db20d08ce769fc27e2acc13a7f4a6252873 🚀

--- _next/static/1yFyPpbYGPuvDJbBQUBcL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/39-844336bf160bafe5.js","static/css/15634c3fcf06c2e1.css","static/chunks/pages/index-c6985f2a773c4ec6.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
